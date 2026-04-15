1fb62540a8cdb05ea3069128fe1d18f6ca369c7a
ellyhiya <ellyhiya@gmail.com>
2026-04-15 10:41:12 +0700

    Login logic done

--- app/src/main/java/com/example/projectanmp/LoginFragment.kt
+++ app/src/main/java/com/example/projectanmp/LoginFragment.kt
@@ -5,14 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.projectanmp.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
+    private lateinit var binding: FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding= FragmentLoginBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnLogin.setOnClickListener {
+            val username=binding.editUsername.text.toString().trim()
+            val password=binding.editPsw.text.toString().trim()
+            if (username == "student" && password == "123") {
+                Toast.makeText(requireContext(),"Login Berhasil", Toast.LENGTH_SHORT).show()
+
+                val actionLoginToDashboard= LoginFragmentDirections.actionLoginToDashboard()
+                findNavController().navigate(actionLoginToDashboard)
+
+            } else {
+                Toast.makeText(requireContext(), "Username atau password salah", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/projectanmp/MainActivity.kt
+++ app/src/main/java/com/example/projectanmp/MainActivity.kt
@@ -8,28 +8,14 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.ui.NavigationUI
 import com.example.projectanmp.databinding.ActivityMainBinding
-import kotlin.io.root
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
-        val view=binding.root
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(view)
-        // buat bottomNavBar
-        navController = (supportFragmentManager.findFragmentById(R.id.fragmentHost) as NavHostFragment).navController
-        NavigationUI.setupActionBarWithNavController(this, navController)
-//        binding.bottomNav.setupWithNavController(navController)
-//
-//        // buat navigation samping
-//        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
-//        NavigationUI.setupWithNavController(binding.navView, navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
-
     }
 }
